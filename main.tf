d1af0bd058c00858bf3a95777d027138a44bed6a
Marcelo Olivas <mfolivas@gmail.com>
2021-12-01 23:38:49 -0500

    Internal - added vpc and subnet

--- main.tf
+++ main.tf
@@ -2,35 +2,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "cidr_blocks" {
-  description = "cidr blocks for vpc and subnets"
-  type = list(string)
-}
+variable vpc_cidr_blocks {}
+variable subnet_cidr_blocks {}
+variable avail_zone {}
+variable env_prefix {}
 
-variable "environment" {
-  description = "environment name"
-}
-resource "aws_vpc" "development-vpc" {
-  cidr_block = var.cidr_blocks[0]
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_blocks
   tags = {
-    Name: var.environment
-    vpc_env: "dev"
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.development-vpc.id
-  cidr_block = var.cidr_blocks[1]
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_blocks
+  availability_zone = var.avail_zone
   tags = {
-    Name: "subtet-1dev"
+    Name: "${var.env_prefix}-subnet-1"
   }
-}
-
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
-}
-
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
 }
