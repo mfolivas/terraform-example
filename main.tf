f67f9382208b4a9c692525e594f68524367548be
Marcelo Olivas <mfolivas@gmail.com>
2021-12-06 22:27:12 -0500

    Internal - added the state via s3

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "terraform-myapp-bucket"
+    key = "myapp/state.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
